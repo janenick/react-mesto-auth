ab15d7e4a929c1fcf290e93a788ab1271989c051
Evgeniya Nikitina <janebornick@yandex.ru>
2020-12-03 12:25:54 +0300

    Updates

---
+++ precache-manifest.5cb1f795117c90e850767f6006a115c5.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e05af361457b80d95c2c07d3383ed78",
+    "url": "./index.html"
+  },
+  {
+    "revision": "7b05b311fdf7b4d8561c",
+    "url": "./static/css/main.880cec79.chunk.css"
+  },
+  {
+    "revision": "ad67698c49c20854f0c3",
+    "url": "./static/js/2.7381b563.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "./static/js/2.7381b563.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b05b311fdf7b4d8561c",
+    "url": "./static/js/main.207382b2.chunk.js"
+  },
+  {
+    "revision": "8eba28d8ada4efb7ebaa",
+    "url": "./static/js/runtime-main.cb0fdd19.js"
+  },
+  {
+    "revision": "09f4068bb74fc78ffbce74f5596d6aa5",
+    "url": "./static/media/Inter-Black.09f4068b.woff2"
+  },
+  {
+    "revision": "e37354838bf8078ac0d296217613533f",
+    "url": "./static/media/Inter-Black.e3735483.woff"
+  },
+  {
+    "revision": "7a8cc7241f766a142e15b2948804e547",
+    "url": "./static/media/Inter-Medium.7a8cc724.woff"
+  },
+  {
+    "revision": "f6cf0a0bc5fce3307e2c426eb14eb752",
+    "url": "./static/media/Inter-Medium.f6cf0a0b.woff2"
+  },
+  {
+    "revision": "4dd66a113d54a7f9a1ae913049610617",
+    "url": "./static/media/Inter-Regular.4dd66a11.woff2"
+  },
+  {
+    "revision": "7c539936c4c8c822b59a1bcc6c08a6ec",
+    "url": "./static/media/Inter-Regular.7c539936.woff"
+  },
+  {
+    "revision": "9fdebb6b009c16af50fae58657e30a94",
+    "url": "./static/media/btn-heart-filled.9fdebb6b.svg"
+  },
+  {
+    "revision": "74771c85e32e79c73b64ebd93ef1fbf2",
+    "url": "./static/media/btn-heart.74771c85.svg"
+  },
+  {
+    "revision": "bdfb8ebb113b9312e080c2f4b4213223",
+    "url": "./static/media/btn-plus.bdfb8ebb.svg"
+  },
+  {
+    "revision": "c7df05485663b2b3ec3b99326f0f5ea2",
+    "url": "./static/media/close-icon.c7df0548.svg"
+  },
+  {
+    "revision": "58afbe73d6547d1730a443f381c68d9e",
+    "url": "./static/media/edit_button.58afbe73.svg"
+  },
+  {
+    "revision": "855a8c9865e79463f31e74f9abcb4438",
+    "url": "./static/media/header-logo.855a8c98.svg"
+  },
+  {
+    "revision": "4072126c596bd9485c6e9601f6d4d35d",
+    "url": "./static/media/sing-fail.4072126c.svg"
+  },
+  {
+    "revision": "e38b395c190826b3bf6d78baff1415a0",
+    "url": "./static/media/sing-success.e38b395c.svg"
+  },
+  {
+    "revision": "9c3b03ae735a14dc757b4d25c0ba5fb3",
+    "url": "./static/media/trash.9c3b03ae.svg"
+  }
+]);
